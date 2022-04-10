2aa4db27e2c063ec93124a5726bbecb978a083df
Kamil Krzyśków <kamilzary@gmail.com>
2022-04-10 14:47:42 +0200

    added a ProxyFix

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+from werkzeug.middleware.proxy_fix import ProxyFix
 import os
 
 app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app)
 limiter = Limiter(app, key_func=get_remote_address)
 
 
@@ -21,6 +23,7 @@ def myip():
         request_method=request.method,
         remote_address=get_remote_address(),
         os_pid=os.getpid(),
+        request_host=request.environ["HTTP_HOST"],
         request_remote=request.environ["REMOTE_ADDR"],
         request_forward=request.environ['HTTP_X_FORWARDED_FOR']
     )
