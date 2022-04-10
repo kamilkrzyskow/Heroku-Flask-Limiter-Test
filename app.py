d9611f09d47091829f765a7a9c305d4863b1393a
Kamil Krzyśków <kamilzary@gmail.com>
2022-04-10 14:09:20 +0200

    fixed double route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
@@ -6,13 +6,7 @@ app = Flask(__name__)
 limiter = Limiter(app, key_func=get_remote_address)
 
 
-@app.route("/myip")
+@app.route("/myip", methods=["GET", "POST"])
 @limiter.limit("10/minute")
 def myip():
-    return f"GET {get_remote_address()}"
-
-
-@app.route("/myip", methods="POST")
-@limiter.limit("10/minute")
-def myip():
-    return f"POST {get_remote_address()}"
+    return f"{request.method} {get_remote_address()}"
